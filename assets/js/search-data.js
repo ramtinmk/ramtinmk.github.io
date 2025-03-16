e0f52c554a3a60073622d138f2b0b261aaaa420f
ramtinmk <ramtinmk1382@gmail.com>
2025-03-16 14:48:56 +0000

    Deploying to gh-pages from @ ramtinmk/ramtinmk.github.io@899737547e0078eb74793503a5f8c206c284ede5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -490,7 +490,14 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%72%61%6D%74%69%6E%6D%6B%31%33%38%32@%67%6D%61%69%6C.%63%6F%6D", "_blank");
+        },
+      },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/ramtinmk", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -498,6 +505,13 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://inspirehep.net/authors/1010907", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/https://www.linkedin.com/in/ramtin-mahmoudi-6193b51a1/", "_blank");
         },
       },{
         id: 'social-rss',
@@ -512,13 +526,6 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
         },
       },{
       id: 'light-theme',
